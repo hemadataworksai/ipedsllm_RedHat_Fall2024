29f947b766c6787bcc6e11c04acebd91b3b2a1e4
enggabhishek <abhiemin@gmail.com>
2024-10-17 00:24:56 -0400

    update

---
+++ python_chatbot_app/chroma_vector_db/app.py
@@ -0,0 +1,66 @@
+import chromadb
+from chromadb.config import Settings
+from langchain_community.document_loaders import JSONLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import os
+from uuid import uuid4
+from dotenv import load_dotenv
+load_dotenv()
+
+CHROMADB_HOST = os.getenv('CHROMADB_HOST')
+CHROMADB_PORT = os.getenv('CHROMADB_PORT')
+settings = Settings(anonymized_telemetry=False, allow_reset=True)
+BASE_PATH = '/api/v1'
+FULL_URL = f"http://{CHROMADB_HOST}:{CHROMADB_PORT}{BASE_PATH}"
+
+try:
+    client = chromadb.HttpClient(
+        host=FULL_URL, settings=settings)
+
+    collection_name = os.getenv('COLLECTION_NAME')
+    collection = client.get_or_create_collection(collection_name)
+
+    def metadata_func(record: dict, metadata: dict) -> dict:
+        def column_retriever(ls):
+            cname = []
+            dtype = []
+            cdesc = []
+            for i in range(len(ls)):
+                cname.append(record.get("Columns")[i].get("Column_Name"))
+                dtype.append(record.get("Columns")[i].get("Data_Type"))
+                cdesc.append(record.get("Columns")[
+                             i].get("Column_Description"))
+            return cname, dtype, cdesc
+        cname, dtype, cdesc = column_retriever(record.get("Columns"))
+
+        metadata["Table_Name"] = record.get("Table_Name")
+        metadata["Table_Description"] = record.get("Table_Description")
+        metadata["Column_Names"] = str(cname)
+        metadata["Data_Type"] = str(dtype)
+        metadata["Column_Description"] = str(cdesc)
+        return metadata
+    
+    openai_api_key = os.getenv("OPENAI_API_KEY")  # Adjust if necessary
+    embedding_function = OpenAIEmbeddings(
+        openai_api_key=openai_api_key, model=os.getenv('TEXT_EMBEDDING'))
+
+    file_path = os.getenv('JSON_FILE_PATH')
+    loader = JSONLoader(
+        file_path=file_path,
+        jq_schema=".[].Table_Info[]",
+        content_key="Table_Name",
+        metadata_func=metadata_func,
+    )
+    data = loader.load()
+    
+    vectorstore = Chroma(client=client, collection_name=collection_name,
+                     embedding_function=embedding_function)
+    uuids = [str(uuid4()) for _ in range(len(data))]
+    
+    vectorstore.add_documents(documents=data, ids=uuids)
+
+    print("Data loaded successfully into ChromaDB collection:", collection_name)
+
+except Exception as e:
+    print("Error:", e)
---
+++ python_chatbot_app/langchain_bot/chroma_retriever.py
@@ -0,0 +1,43 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+import chromadb
+from chromadb.config import Settings
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+collection_name = os.getenv('COLLECTION_NAME')
+CHROMADB_HOST = os.getenv('CHROMADB_HOST')
+CHROMADB_PORT = os.getenv('CHROMADB_PORT')
+settings = Settings(anonymized_telemetry=False, allow_reset=True)
+
+client = chromadb.HttpClient(
+    host=CHROMADB_HOST, port=CHROMADB_PORT, settings=settings)
+
+print("==========CHROMADB CONNECTION ESTABLISHED==============")
+embedding_function = OpenAIEmbeddings(
+    openai_api_key=OPENAI_API_KEY, model=os.getenv('TEXT_EMBEDDING')
+)
+
+vectorstore = Chroma(client=client, collection_name=collection_name,
+                     embedding_function=embedding_function)
+
+retriever = vectorstore.as_retriever()
+
+# Define your template
+template = """Answer the question based only on the following context:
+{context}
+Search for the table descriptions in the context and accordingly search for column names and associated column description. Include only relevant tables and columns which can be used by the downstream Text-to-SQL Agent to create SQL Queries for generating answer.
+Search for any information performing the following tasks:
+1. Table Names
+2. Table Descriptions
+3. Column Names
+4. Column Descriptions
+5. Encoded Values
+Finally, only return table names, column names and Encoded Values only (if available).
+
+Question: {question}
+"""
+retriever_prompt = ChatPromptTemplate.from_template(template)
